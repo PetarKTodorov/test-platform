f2d99d767da249bb52313b5e5a093c2df674fd91
PetarKTodorov <PetarKTodorov@gmail.com>
2023-04-16 23:30:08 +0300

    Add Index page

--- src/Application/TestPlatform.Application/Controllers/HomeController.cs
+++ src/Application/TestPlatform.Application/Controllers/HomeController.cs
@@ -1,27 +1,12 @@
 ﻿namespace TestPlatform.Application.Controllers
 {
-    using System.Diagnostics;
-
     using Microsoft.AspNetCore.Mvc;
-
-    using TestPlatform.Application.Models;
 
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return this.View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
