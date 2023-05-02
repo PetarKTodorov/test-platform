fd1b94aca8202d69fc7306f9e803fad3ab19c86a
PetarKTodorov <PetarKTodorov@gmail.com>
2023-05-02 20:00:19 +0300

    Add Model state validation attribute

---
+++ src/Application/TestPlatform.Application.Infrastructures/Filtres/ValidateModelStateAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace TestPlatform.Application.Infrastructures.Filtres
+{
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        private const string FILTER_MODEL = "model";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                object model = context.ActionArguments
+                    .FirstOrDefault(arg => arg.Key.ToLower().Contains(FILTER_MODEL))
+                    .Value;
+
+                Controller controller = context.Controller as Controller;
+                if (controller != null)
+                {
+                    ViewResult view = controller.View(model);
+
+                    context.Result = view;
+                }
+            }
+        }
+    }
+}
---
+++ src/DTOs/TestPlatform.DTOs.BindingModels/User/CreateUserBM.cs
@@ -0,0 +1,38 @@
+﻿namespace TestPlatform.DTOs.BindingModels.User
+{
+    using TestPlatform.Services.Mapper.Interfaces;
+    using TestPlatform.Database.Entities.Authorization;
+    using System.ComponentModel.DataAnnotations;
+    using TestPlatform.Common.Constants;
+
+    public class CreateUserBM : IMapTo<User>
+    {
+        [Required]
+        [EmailAddress]
+        [RegularExpression(Validations.EMAIL_REGEX, ErrorMessage = "This is invalid email format.")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: Validations.TWO_POWER_EIGHT, MinimumLength = Validations.ONE)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: Validations.TWO_POWER_EIGHT, MinimumLength = Validations.ONE)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: Validations.TWO_POWER_EIGHT, MinimumLength = Validations.ONE)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(Validations.PASSWORD_REGEX, ErrorMessage = "Password field must be 10 symbols and contains at least one: <ul><li>Capital letter</li></ul>")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
