c4f2d9f43b93b5503af2b1b0a23d7b465ed3f58b
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-14 14:53:18 +0300

    Update select2-config.js

--- src/Application/TestPlatform.Application/Webpack/src/js/configurations/select2-config.js
+++ src/Application/TestPlatform.Application/Webpack/src/js/configurations/select2-config.js
@@ -1,8 +1,13 @@
 $(() => {
-    // initialize globally select2
-    $('.js-select2').select2();
+    configSelect2();
+    $(document).on('form-reload', configSelect2);
 
-    $('.js-select2-modal').select2({
-        dropdownParent: $('.js-modal-parent')
-    });
+    function configSelect2() {
+        // initialize globally select2
+        $('.js-select2').select2();
+
+        $('.js-select2-modal').select2({
+            dropdownParent: $('.js-modal-parent')
+        });
+    }
 });
