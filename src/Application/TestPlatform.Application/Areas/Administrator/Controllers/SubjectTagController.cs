fbabb0c2c72a30caecb1711c84b3ff85071fc121
PetarKTodorov <PetarKTodorov@gmail.com>
2023-05-15 19:39:08 +0300

    Add list of all subject tags

---
+++ src/Application/TestPlatform.Application/Areas/Administrator/Controllers/SubjectTagController.cs
@@ -0,0 +1,79 @@
+﻿namespace TestPlatform.Application.Areas.Administrator.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using TestPlatform.DTOs.ViewModels.Common;
+    using TestPlatform.DTOs.ViewModels.SubjectTags;
+    using TestPlatform.Services.Database.Subjects.Interfaces;
+
+    public class SubjectTagController : BaseAdministratorController
+    {
+        private readonly ISubjectTagService subjectTagService;
+
+        public SubjectTagController(ISubjectTagService subjectTagService)
+        {
+            this.subjectTagService = subjectTagService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListAll(int? page = 1)
+        {
+            if (page != null && page < 1)
+            {
+                page = 1;
+            }
+
+            var result = new PageableResult<AllSubjectTagsVM>();
+            var subjectTags = await this.subjectTagService.FindAllAsync<AllSubjectTagsVM>(page.Value, result.PageSize);
+            var subjectTagsCount = await this.subjectTagService.GetCountOfAllAsyns();
+
+            result.Results = subjectTags;
+            result.AllResultsCount = subjectTagsCount;
+            result.CurrentPage = page.Value;
+
+            return this.View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(int dumy)
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int dumy)
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int dumy)
+        {
+            return this.View();
+        }
+    }
+}
---
+++ src/DTOs/TestPlatform.DTOs.ViewModels/SubjectTags/AllSubjectTagsVM.cs
@@ -0,0 +1,17 @@
+﻿namespace TestPlatform.DTOs.ViewModels.SubjectTags
+{
+    using System.ComponentModel;
+
+    using TestPlatform.Database.Entities.Subjects;
+    using TestPlatform.Services.Mapper.Interfaces;
+
+    public class AllSubjectTagsVM : IMapFrom<SubjectTag>
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Is Deleted")]
+        public bool IsDeleted { get; set; }
+
+        public string Name { get; set; }
+    }
+}
