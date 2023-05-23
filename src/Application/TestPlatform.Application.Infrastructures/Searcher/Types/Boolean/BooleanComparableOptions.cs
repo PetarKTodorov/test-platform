c79973a6b76b856e523bbce8db1dd35a26728846
PetarKTodorov <PetarKTodorov@gmail.com>
2023-05-23 20:28:29 +0300

    Add boolean type search criteria

---
+++ src/Application/TestPlatform.Application.Infrastructures/Searcher/Types/Boolean/BooleanComparableOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace TestPlatform.Application.Infrastructures.Searcher.Types.Boolean
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum BooleanComparableOptions
+    {
+        [Display(Name = "Equals")]
+        Equal,
+    }
+}
