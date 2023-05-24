f107ba47c01f7c5fdb5d76b863d591467d92381e
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-24 15:54:48 +0300

    Update auto mapper config

--- src/Services/TestPlatform.Services.Managers/UserManager.cs
+++ src/Services/TestPlatform.Services.Managers/UserManager.cs
@@ -6,7 +6,6 @@
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Authentication.Cookies;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.VisualBasic;
     using TestPlatform.Application.Infrastructures.ApplicationUser;
     using TestPlatform.Common.Constants;
     using TestPlatform.Database.Entities.Authorization;
