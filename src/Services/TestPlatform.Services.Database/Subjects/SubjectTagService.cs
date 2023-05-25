d8f731254754eaf3fcd45ffc5cd266ae1cfc3183
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-25 21:35:56 +0300

    Delete user subject tags on subject tag deletion

--- src/Services/TestPlatform.Services.Database/Subjects/SubjectTagService.cs
+++ src/Services/TestPlatform.Services.Database/Subjects/SubjectTagService.cs
@@ -1,5 +1,7 @@
 ﻿namespace TestPlatform.Services.Database.Subjects
 {
+    using System;
+    using System.Threading.Tasks;
     using AutoMapper;
 
     using TestPlatform.Database.Entities.Subjects;
@@ -8,11 +10,35 @@
 
     public class SubjectTagService : BaseService<SubjectTag>, ISubjectTagService
     {
-        public SubjectTagService(IBaseRepository<SubjectTag> baseRepository, IMapper mapper)
+        private readonly IUserSubjectTagMapService userSubjectTagMapService;
+
+        public SubjectTagService(IBaseRepository<SubjectTag> baseRepository,
+            IMapper mapper,
+            IUserSubjectTagMapService userSubjectTagMapService)
             : base(baseRepository, mapper)
         {
+            this.userSubjectTagMapService = userSubjectTagMapService;
         }
 
+        public override async Task<T> DeleteAsync<T>(Guid id, Guid currentUserId)
+        {
+            var resultFromDelete = await base.DeleteAsync<T>(id, currentUserId);
 
+            await this.HardDeleteUserSubjectTagsMapAsync(id);
+
+            return resultFromDelete;
+        }
+
+        private async Task HardDeleteUserSubjectTagsMapAsync(Guid subjectTagId)
+        {
+            var userSubjectTags = this.userSubjectTagMapService.FindAllAsQueryable<UserSubjectTagMap>()
+                .Where(ustm => ustm.SubjectTagId == subjectTagId)
+                .ToArray();
+
+            foreach (var userSubjectTag in userSubjectTags)
+            {
+                await this.userSubjectTagMapService.HardDeleteAsync<UserSubjectTagMap>(userSubjectTag.Id);
+            }
+        }
     }
 }
