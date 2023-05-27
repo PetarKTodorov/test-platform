751303aac5dbbcf573712f20c3b17c1c2cf7b6f4
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-27 16:23:16 +0300

    Create Question Service

---
+++ src/Services/TestPlatform.Services.Database/Questions/QuestionService.cs
@@ -0,0 +1,15 @@
+﻿namespace TestPlatform.Services.Database.Questions
+{
+    using AutoMapper;
+    using TestPlatform.Database.Entities.Questions;
+    using TestPlatform.Database.Repositories.Interfaces;
+    using TestPlatform.Services.Database.Questions.Interfaces;
+
+    public class QuestionService : BaseService<Question>, IQuestionService
+    {
+        public QuestionService(IBaseRepository<Question> baseRepository, IMapper mapper)
+            : base(baseRepository, mapper)
+        {
+        }
+    }
+}
