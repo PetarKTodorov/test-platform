965e47bcf24386fa53d7df427bbe9564feb50848
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-28 12:19:40 +0300

    Don't allow to delete subject tag if it is assigned to something

--- src/DTOs/TestPlatform.DTOs.ViewModels/Subjects/ListSubjectTagsVM.cs
+++ src/DTOs/TestPlatform.DTOs.ViewModels/Subjects/ListSubjectTagsVM.cs
@@ -16,5 +16,14 @@
 
         [CustomSearchField]
         public string Name { get; set; }
+
+        [DisplayName("Users with this subject tag")]
+        public int UsersCount { get; set; }
+
+        [DisplayName("Questions with this subject tag")]
+        public int QuestionsCount { get; set; }
+
+        [DisplayName("Tests with this subject tag")]
+        public int TestsCount { get; set; }
     }
 }
