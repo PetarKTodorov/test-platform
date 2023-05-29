e85cdca2b53a2452bd85af7a6576b3fbca6f7e77
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-29 18:27:20 +0300

    Create UidAttribute and EnumExtensions

---
+++ src/Common/TestPlatform.Common/Attributes/UidAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace TestPlatform.Common.Attributes
+{
+    using System;
+    using TestPlatform.Common.Constants;
+
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    public class UidAttribute : Attribute
+    {
+        public Guid Uid = Guid.Empty;
+
+        public UidAttribute(string uid)
+        {
+            if (!Guid.TryParse(uid, out Uid))
+            {
+                throw new Exception(string.Format(ExceptionMessages.CAN_NOT_PARSE_UID, uid));
+            }
+        }
+    }
+}
