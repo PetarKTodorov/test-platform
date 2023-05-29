8420c3a336f99d183adb74c76753b4b5a92f9a4e
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-05-29 23:08:33 +0300

    Preserve the answer when changing question type

--- src/Application/TestPlatform.Application/Webpack/src/js/answers/add-remove-answers-from-question.js
+++ src/Application/TestPlatform.Application/Webpack/src/js/answers/add-remove-answers-from-question.js
@@ -1,9 +1,63 @@
 (() => {
     const addAnswerButton = $(".js-add-answer");
     const removeAnswerButton = $(".js-remove-answer");
+    const questionForm = $(".js-question-form");
+    const questionTypeIdSelect = $("#QuestionTypeId");
 
+    const mutlipleChoiceQuestionTypeId = "AE825D9C-EF76-45E9-A31C-561976E6387E";
+    const singleChoiceQuestionTypeId = "00F1EA00-27B4-4894-8AFE-877C36B9E41D";
+
+    questionTypeIdSelect.on("change", onChangeQuestionType);
+    questionForm.one("submit", updateSubmitQuestionForm);
     removeAnswerButton.on("click", removeAnswer);
     addAnswerButton.on("click", addAnswer);
+
+    function onChangeQuestionType() {
+        const questionTypeId = questionTypeIdSelect.val();
+        if (questionTypeId.toLowerCase() == singleChoiceQuestionTypeId.toLowerCase()) {
+            const answerCheckboxes = $('.js-answer-checkbox');
+
+            answerCheckboxes.each(function () {
+                const checkboxElement = $(this);
+                const checkboxName = checkboxElement.attr("name");
+
+                const checkBoxParent = checkboxElement.parent();
+
+                checkboxElement.remove();
+                checkBoxParent.prepend(generateInputBasedOnQuestionType(questionTypeId, checkboxName, checkboxName));
+            });
+        } else {
+            const answerRadios = $('.js-answer-radio');
+
+            answerRadios.each(function () {
+                const radioElement = $(this);
+                const radioValue = radioElement.attr("value");
+
+                const radioParent = radioElement.parent();
+
+                radioElement.remove();
+                radioParent.prepend(generateInputBasedOnQuestionType(questionTypeId, radioValue, radioValue));
+            });
+        }
+    }
+
+    function updateSubmitQuestionForm(event) {
+        event.preventDefault();
+
+        const questionTypeId = questionTypeIdSelect.val();
+        if (questionTypeId.toLowerCase() == singleChoiceQuestionTypeId.toLowerCase()) {
+            const selectedRadioButtonValue = $('input[name="IsCorrect"]:checked').val();
+            const trueCheckboxHtml = $(`
+                <input id="${selectedRadioButtonValue}" checked="checked" name="${selectedRadioButtonValue}" type="checkbox" value="true" hidden />
+            `);
+
+            trueCheckboxHtml.insertBefore(addAnswerButton.parent());
+        }
+
+        questionForm.trigger("submit");
+
+        return true;
+    }
 
     function removeAnswer() {
         const removeButton = $(this);
@@ -31,11 +85,8 @@
             .find(".js-answer-container")
             .length;
 
-        const lastAnswer = parentContainer
-            .find(".js-answer-container")
-            .last();
-
-        const answerHtml = generateAnswerHtml(index);
+        const questionTypeId = questionTypeIdSelect.val();
+        const answerHtml = generateAnswerHtml(index, questionTypeId);
 
         const newAnswer = $(answerHtml);
         const buttonsContainer = addButton
@@ -43,11 +94,11 @@
         newAnswer.insertBefore(buttonsContainer);
     };
 
-    function generateAnswerHtml(index) {
+    function generateAnswerHtml(index, typeId) {
         const idNameAttribute = `Answers[${index}].Id`;
         const answerIdNameAttribute = `Answers[${index}].AnswerId`;
-        const isCorrectNameAttribute = `Answers[${index}].IsCorrect`;
         const isCorrectInputId = `Answers_${index}__.IsCorrect`;
+        const isCorrectNameAttribute = `Answers[${index}].IsCorrect`;
         const answerContentNameAttribute = `Answers[${index}].AnswerContent`;
 
         const answerHtml = `
@@ -55,12 +106,27 @@
                 <input hidden type="text" name="${idNameAttribute}" value="" />
                 <input hidden type="text" name="${answerIdNameAttribute}" value="" />
                 <div class="d-flex gap-2">
-                    <input id="${isCorrectInputId}" name="${isCorrectNameAttribute}" type="checkbox" value="true">
-                    <input id="${isCorrectInputId}" name="${isCorrectNameAttribute}" type="checkbox" value="false" hidden>
+                    ${generateInputBasedOnQuestionType(typeId, isCorrectNameAttribute, isCorrectInputId)}
                     <input class="form-control" type="text" name="${answerContentNameAttribute}" value="" />
                 </div>
             </div>`;
 
         return answerHtml;
+    }
+
+    function generateInputBasedOnQuestionType(questionTypeId, isCorrectNameAttribute, isCorrectInputId) {
+        let inputHtml = "";
+
+        if (questionTypeId.toLowerCase() == mutlipleChoiceQuestionTypeId.toLowerCase()) {
+            inputHtml = `
+                <input class="js-answer-checkbox" id="${isCorrectInputId}" name="${isCorrectNameAttribute}" type="checkbox" value="true">
+                <input id="${isCorrectInputId}" name="${isCorrectNameAttribute}" type="checkbox" value="false" hidden>
+            `;
+
+        } else if (questionTypeId.toLowerCase() == singleChoiceQuestionTypeId.toLowerCase()) {
+            inputHtml = `<input class="js-answer-radio" id="${isCorrectInputId}" name="IsCorrect" type="radio" value="${isCorrectNameAttribute}" />`;
+        }
+
+        return inputHtml;
     }
 })();
